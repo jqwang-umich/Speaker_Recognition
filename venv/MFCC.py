d4d0305a5a5e875cbea118f6aff95ae6d28dbcfa
jqwang <jqwang.umich@gmail.com>
2018-12-10 17:00:08 -0500

    initialize the testing and training label/data and save them as npy file

--- venv/MFCC.py
+++ venv/MFCC.py
@@ -4,10 +4,11 @@ import tensorflow as tf
 import os
 import shutil
 
-def mfcc(fileName):
+def mfcc(fileName, nMFCC):
     # all of the test records are sampled with rate 22050
-    y, sr = librosa.load('sa1.wav')
-    return librosa.feature.mfcc(y=y, sr=sr,n_mfcc=13)
+    y, sr = librosa.load(fileName)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr,n_mfcc=nMFCC)
+    return mfcc
 
 def initialFiles(originalTimitDir):
     listOfFolders = os.listdir(originalTimitDir)
@@ -24,6 +25,40 @@ def initialFiles(originalTimitDir):
                 fileCount += 1
         count += 1
 
+def initialTrainingAndTestingDataForCNN(nMFCC, trainNum, trainSetColumnNum):
+    listOfTrainingData = []
+    listOfTrainingLabel = []
+    listOfTestingData = []
+    listOfTestingLabel = []
+
+    for countOfSpeaker in range(11):
+        for i in range(trainNum):
+            # training file number is a given parameter
+            file = "wavFiles/" + str(countOfSpeaker)+ "/"+ str(i)+ ".wav"
+            mfccSet = mfcc(file,nMFCC)
+            row,column = mfccSet.shape
+            for t in range(column - trainSetColumnNum):
+                slide = mfccSet[:,t : t + trainSetColumnNum]
+                listOfTrainingData.append(slide)
+                listOfTrainingLabel.append(countOfSpeaker)
+
+        for i in range(trainNum,10):
+            file = "wavFiles/" + str(countOfSpeaker) + "/" + str(i) + ".wav"
+            mfccSet = mfcc(file, nMFCC)
+            row, column = mfccSet.shape
+            for t in range(column - trainSetColumnNum):
+                slide = mfccSet[ :, t: t + trainSetColumnNum ]
+                listOfTestingData.append(slide)
+                listOfTestingLabel.append(countOfSpeaker)
+
+
+    np.save("trainingData.npy",listOfTrainingData)
+    np.save("trainingLabel.npy",listOfTrainingLabel)
+    np.save("testingData.npy", listOfTestingData)
+    np.save("testingLabel.npy", listOfTestingLabel)
+
 
 # os.mkdir("wavFiles")
 # initialFiles("F:\武汉大学\物联网\学习\\2016-2017\毕业设计\\timit_test_1-3\dr1")
+# mfcc("wavFiles/0/0.wav",13)
+initialTrainingAndTestingDataForCNN(13,8,13)
